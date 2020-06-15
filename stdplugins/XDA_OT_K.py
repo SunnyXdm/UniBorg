c321878fd89330f186470987446ac9694035d018
kashish <45434553+SunnyXdm@users.noreply.github.com>
2020-06-15 12:45:50 +0530

    Add files via upload

---
+++ stdplugins/XDA_OT_K.py
@@ -0,0 +1,16 @@
+from collections import defaultdict, deque
+import re
+
+import regex
+from telethon import events, utils
+from telethon.tl import types, functions
+
+@borg.on(events.NewMessage(pattern=r"#SOL_irl", incoming=True, chats=-1001055527387))
+async def _(event):
+	if event.reply_to_msg_id:
+		message_id = event.reply_to_msg_id
+		await borg.send_message(
+                    event.chat_id,
+                    'Sent:',
+                    reply_to=message_id,
+                )
---
+++ stdplugins/XDA_OT_spearwolf.py
@@ -0,0 +1,12 @@
+from collections import defaultdict, deque
+import re
+
+import regex
+from telethon import events, utils
+from telethon.tl import types, functions
+
+@borg.on(events.NewMessage(pattern=r"#SOL_irl", incoming=True, chats=-1001055527387))
+async def _(event):
+	if event.reply_to_msg_id:
+		message_id = event.reply_to_msg_id
+		await borg.send_message("@spearwolf", "https://t.me/xdaofftopicgroup/{}".format(message_id))
